35c1c8e1fbf7f04abd0a6adfe4633c5861cc30e9
Jheferson Reis <jhefersonvds@gmail..com>
2025-11-05 16:45:55 -0300

    feat: add animated splash screen with lottie files

--- lib/src/constants/routes_constants.dart
+++ lib/src/constants/routes_constants.dart
@@ -1,4 +1,5 @@
 sealed class RoutesConstants {
+  static const String splash = '/splash';
   static const String home = '/home';
   static const String pokemonInfo = '/pokemonInfo/:pokemonName';
 }
--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -1,16 +1,41 @@
+import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:im_mottu_mobile/src/constants/routes_constants.dart';
 import 'package:im_mottu_mobile/src/pages/pokemon_detail_page.dart';
+import 'package:im_mottu_mobile/src/pages/splash_page.dart';
 
 import 'pages/home_page.dart';
 
 final GoRouter router = GoRouter(
-  initialLocation: RoutesConstants.home,
+  initialLocation: RoutesConstants.splash,
   routes: [
+    GoRoute(
+      path: RoutesConstants.splash,
+      name: RoutesConstants.splash,
+      pageBuilder: (context, state) => CustomTransitionPage(
+        key: state.pageKey,
+        child: const SplashPage(),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          final slideOut = Tween<Offset>(
+            begin: Offset.zero,
+            end: const Offset(-1.0, 0.0),
+          ).animate(CurvedAnimation(parent: secondaryAnimation, curve: Curves.easeInOut));
+          return SlideTransition(position: slideOut, child: child);
+        },
+      ),
+    ),
     GoRoute(
       path: RoutesConstants.home,
       name: RoutesConstants.home,
-      builder: (context, state) => const HomePage(),
+      pageBuilder: (context, state) => CustomTransitionPage(
+        key: state.pageKey,
+        child: const HomePage(),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          final tween = Tween<Offset>(begin: const Offset(1.0, 0.0), end: Offset.zero);
+          final curved = CurvedAnimation(parent: animation, curve: Curves.easeInOut);
+          return SlideTransition(position: tween.animate(curved), child: child);
+        },
+      ),
     ),
     GoRoute(
       path: RoutesConstants.pokemonInfo,
